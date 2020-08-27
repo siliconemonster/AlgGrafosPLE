2ae74d52a61ce573ba421e8786d76a5befd3a031
Aline <alinefrez@netscape.net>
2020-08-27 20:57:50 -0300

    Atualizei as chamadas dos métodos

--- 01/AlgGrafos.java
+++ 01/AlgGrafos.java
@@ -1,13 +1,19 @@
 public class AlgGrafos {
     public static void main(String args[]) {
-        
+
         Graph g1 = new Graph();
-        g1.add_vertex();
-        g1.add_vertex();
-        g1.add_vertex();
+        g1.add_vertex( 1 );
+        g1.add_vertex( 2 );
+        g1.add_vertex( 3 );
+        g1.add_vertex( 4 );
+
         g1.add_edge(1,2);
         g1.add_edge(1,3);
+        g1.add_arc(1,4);
+
         g1.print();
-        
+
+        Graph g2 = g1.subjacent();
+        g2.print();
     }
 }
