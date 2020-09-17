8bbba865d9059b35593a80017eb2c47e789fe683
Aline <alinefrez@netscape.net>
2020-09-17 11:27:26 -0300

    Atualizei o programa Vertex

--- 01/Vertex.java
+++ 01/Vertex.java
@@ -1,12 +1,18 @@
+import java.io.Serializable;
 import java.util.HashMap;
 
-public class Vertex {
+public class Vertex implements Serializable {
     protected Integer id;
     protected HashMap<Integer,Vertex> nbhood;
+    protected Vertex parent;
+    protected Integer dist;
 
     public Vertex ( int id ) {
+		// id >= 1
         this.id = id;
         nbhood = new HashMap<Integer,Vertex>();
+        parent = null;
+        dist = null;
     }
 
     public void add_neighbor( Vertex viz ) {
@@ -18,9 +24,20 @@ public class Vertex {
         return nbhood.size();
     }
 
+    public void discover( Vertex parent ) {
+        this.parent = parent;
+        this.dist = parent.dist + 1;
+    }
+
     public void print() {
-        System.out.print("\nId do vertice = " + id + ", Vizinhança: " );
+        System.out.print("\nId do vértice " + id + ", Vizinhança: " );
         for( Vertex v : nbhood.values())
             System.out.print(" " + v.id );
+        if( parent != null)
+            System.out.print(", pai " + parent.id + " distância " + dist );
+        else if ( dist == null )
+            System.out.print(", não alcançável pela raiz");
+        else
+            System.out.print(", raiz, distância" + dist);
     }
 }
