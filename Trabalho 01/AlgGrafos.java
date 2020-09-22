ecba8f018a8429b9132a932fd64dcee2ff3cbfe2
Aline <alinefrez@netscape.net>
2020-09-22 19:26:27 -0300

    Adicionei arquivos para trabalho 1

---
+++ Trabalho 01/AlgGrafos.java
@@ -0,0 +1,125 @@
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.util.Scanner;
+import java.util.List;
+
+public class AlgGrafos {
+    public static void main(String args[]) {
+
+        Scanner scan1 = new Scanner(System.in);
+
+        String line1 = "\n\n 0 Sair \n 1 Print \n 2 Ler de arquivo \n 3 Escrever em arquivo \n 4 Adicionar vértice";
+        String line2 = "\n 5 Adicionar aresta \n 6 Excluir vértice \n 7 BFS \n 8 Subjacente \n 9 Compactar";
+        String line3 = "\n 10 DFS \n 11 Ordenação topológica \n 12 Reverter arcos \n 13 CFC";
+        String line4 = "\n 14 Bipartido \n Escolha a opção: ";
+        String menu = line1 + line2 + line3 + line4;
+
+        Digraph g1 = new Digraph();
+
+        while( true ) {
+            System.out.printf( menu );
+            int choice = scan1.nextInt();
+            switch( choice ) {
+                case 0:
+                    return;
+                case 1:
+                    g1.print();
+                    break;
+                case 2:
+                    g1 = read( );
+                    break;
+                case 3:
+                    write( g1 );
+                    break;
+                case 4:
+					           // fazer
+                    g1.add_vertex( 1 );
+                    g1.add_vertex( 2 );
+                    g1.add_vertex( 3 );
+                    g1.add_vertex( 4 );
+                    break;
+                case 5:
+					           // fazer
+                     // g1.add_edge(1,2);
+                     // g1.add_edge(1,3);
+                     // fazer: criar uma opção
+                    g1.add_arc(1,2);
+                    g1.add_arc(1,3);
+                    g1.add_arc(2,3);
+                    g1.add_arc(1,4);
+                    g1.add_arc(3,1);
+                    break;
+                case 6:
+					           System.out.print("Vértice a excluir: ");
+					           int id = scan1.nextInt();
+					           g1.del_vertex( id );
+					           break;
+                case 7:
+					           // fazer
+                    g1.BFS( 4 );
+                    break;
+                case 8:
+                    Graph g2 = g1.subjacent();
+                    g2.print();
+                    break;
+                case 9:
+          					g1.compact( );
+          					break;
+                case 10:
+    					      g1.DFS( null );
+    					      break;
+                case 11:
+    					      List<Vertex> ts_vertex_set = g1.topological_sorting( );
+    					      System.out.printf("\n\n Ordenação topológica \n");
+    					      for ( Vertex v1 : ts_vertex_set )
+    						          System.out.printf("\n id: " + v1.id + " f: " + v1.f );
+    					      break;
+                case 12:
+    					      Digraph d2 = g1.reverse( );
+    					      d2.print( );
+    					      break;
+                case 13:
+    					      g1.CFC( );
+                case 14:
+    					       g1.is_bipartite( );
+    					            break;
+            }
+        }
+    }
+
+    private static void write( Digraph g1 ) {
+        try
+        {
+            FileOutputStream arquivoGrav = new FileOutputStream("myfiles/saida.dat");
+            ObjectOutputStream objGravar = new ObjectOutputStream(arquivoGrav);
+            objGravar.writeObject( g1 );
+            objGravar.flush();
+            objGravar.close();
+            arquivoGrav.flush();
+            arquivoGrav.close();
+            System.out.println("Objeto gravado com sucesso!");
+        }
+        catch(Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static Graph read( ) {
+        Graph g1 = null;
+        try
+        {
+            FileInputStream arquivoLeitura = new FileInputStream("myfiles/saida.dat");
+            ObjectInputStream objLeitura = new ObjectInputStream(arquivoLeitura);
+            g1 = (Graph) objLeitura.readObject();
+            objLeitura.close();
+            arquivoLeitura.close();
+            System.out.println("Objeto recuperado: ");
+        }
+        catch(Exception e) {
+            e.printStackTrace();
+        }
+        return g1;
+    }
+}
---
+++ Trabalho 01/Vertex.java
@@ -0,0 +1,64 @@
+import java.io.Serializable;
+import java.lang.Comparable;
+import java.util.HashMap;
+
+public class Vertex implements Serializable, Comparable<Vertex> {
+    protected Integer id;
+    protected HashMap<Integer,Vertex> nbhood;
+    // parent: refere-se a qual busca?
+    protected Vertex parent, root;
+    protected Integer d, f;
+    // independent set if bipartite: 0, -1, or 1
+    protected int ind_set;
+
+    public Vertex ( int id ) {
+		    // id >= 1
+        this.id = id;
+        nbhood = new HashMap<Integer,Vertex>();
+        //parent = null;
+        //dist = null;
+    }
+
+    @Override public int compareTo( Vertex otherVertex ) {
+  		if( otherVertex.f > this.f)
+  			return 1;
+  		else
+  			return -1;
+  	}
+
+    public void add_neighbor( Vertex viz ) {
+        nbhood.put(viz.id, viz);
+    }
+
+    public int degree() {
+        // grau de saída se direcionado
+        return nbhood.size();
+    }
+
+    public void discover( Vertex parent ) {
+        this.parent = parent;
+        this.d = parent.d + 1;
+    }
+
+    protected Vertex get_root( ) {
+  		if( parent == null )
+  			root = this;
+  		else
+  			root = parent.get_root( );
+  		return root;
+  	}
+
+    public void print() {
+        System.out.print("\nId do vértice " + id + ", Vizinhança: " );
+        for( Vertex v : nbhood.values())
+            System.out.print(" " + v.id );
+        if( d != null )
+    		    System.out.print(". d " + d + ", f " + f );
+        if( parent != null)
+            System.out.print(". Pai " + parent.id + " distância " + d );
+        else if ( d == null )
+            System.out.print(". Não alcançável pela raiz");
+        else
+            System.out.print(". Raiz, distância" + d);
+    }
+}
