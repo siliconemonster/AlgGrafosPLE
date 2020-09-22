6e541bc755d9100302702e7575f763dc647e7557
Aline <alinefrez@netscape.net>
2020-09-22 18:55:02 -0300

    Atualizei o arquivo Graph.java

--- Programa de Aulas/Graph.java
+++ Programa de Aulas/Graph.java
@@ -5,8 +5,15 @@ import java.util.List;
 import java.util.LinkedList;
 import java.util.Queue;
 import java.util.Collections;
+import java.util.Stack;
 
 public class Graph extends Digraph {
+
+    private Stack<Vertex> st1;
+
+    public Graph() {
+    st1 = new Stack<Vertex>( );
+    }
 
     @Override public void add_arc( Integer id1, Integer id2) {
 		System.out.println("Operação não permitida: Adição de arco.");
@@ -14,6 +21,32 @@ public class Graph extends Digraph {
 
 	@Override public boolean is_acyclic( ) {
 		// fazer
+		return true;
+	}
+  @Override public boolean is_bipartite( ) {
+		for( Vertex v1 : vertex_set.values( ) ) {
+			if( v1.ind_set == 0 ) {
+				v1.ind_set = 1;
+				if( ! bipartite_visit( v1 ) )
+					return false;
+			}
+		}
+		System.out.print( "\n É bipartido " );
+		return true;
+	}
+	private boolean bipartite_visit( Vertex v1 ) {
+		for( Vertex v2 : v1.nbhood.values( ) ) {
+			if( v2.ind_set == 0 ) {
+				v2.ind_set = - v1.ind_set;
+				if( ! bipartite_visit( v2 ) )
+					return false;
+			}
+			if( v2.ind_set == v1.ind_set ) {
+				// encontrar um ciclo ímpar
+				System.out.printf( "\n Não é bipartido " + v1.id + " " + v2.id );
+				return false;
+			}
+		}
 		return true;
 	}
 }
